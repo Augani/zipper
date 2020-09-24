08eca689c3beb1e674ba18930ebc4794967d91c7
Augustus Otu <Augani7@gmail.com>
2020-09-24 10:29:15 +0100

    run tests

--- zip_test.go
+++ zip_test.go
@@ -3,17 +3,17 @@ package zipper
 import "testing"
 
 func TestZip(t *testing.T) {
-	var destination, err = ZipIt("C:\\Users\\augan\\Desktop\\western", "", "newWestern")
+	var destination, err = ZipIt("C:\\Users\\augustus\\Desktop\\projects\\goWorkSpace\\src\\github.com\\augani\\musapp", "", "")
 	if err != nil {
 		t.Errorf("There was an error")
 	}
-	if destination != "C:\\Users\\augan\\Desktop\\newWestern.zip" {
+	if destination != "C:\\Users\\augustus\\Desktop\\projects\\goWorkSpace\\src\\github.com\\augani\\musapp.zip" {
 		t.Errorf("Destination was wrong, got: %s, want: %s.", destination, "C:\\Users\\augan\\Desktop\\newWestern.zip")
 	}
 }
 
 func TestUnZip(t *testing.T) {
-	var done, err = UnZipIt("C:\\Users\\augan\\Desktop\\western.zip", "C:\\Users\\augan\\Music")
+	var done, err = UnZipIt("C:\\Users\\augustus\\Desktop\\projects\\goWorkSpace\\src\\github.com\\augani\\musapp.zip", "C:\\Users\\augustus\\Music")
 	if err != nil {
 		t.Errorf("There was an error")
 	}
